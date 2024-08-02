720b3ea710c24a30b9c30b79fda3812ca7933f78
Marcos Netto <marcoscarvalhonetto199@gmail.com>
2024-08-02 17:14:09 -0300

    function expressao lambda declarada

--- src/programacao_funcional_e_expressoes_lambda/function/application/Program.java
+++ src/programacao_funcional_e_expressoes_lambda/function/application/Program.java
@@ -5,6 +5,7 @@ import programacao_funcional_e_expressoes_lambda.function.util.UpperCaseName;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Program {
@@ -17,7 +18,9 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        List<String> names = list.stream().map(Product::nonStaticUpperCaseName).toList();
+        Function<Product, String> func = p -> p.getName().toUpperCase();
+
+        List<String> names = list.stream().map(func).toList();
 
         names.forEach(System.out::println);
 
